7ec23f487298af1331620b59cff836d57c8ed685
Romanov Eugene <effectorea@yandex.ru>
2021-11-07 23:23:59 +0300

    js loading...

--- blocks/scripts/index.js
+++ blocks/scripts/index.js
@@ -11,3 +11,16 @@ const togglePopupVisibility = function() {
 profileEditButtonElement.addEventListener('click', togglePopupVisibility);
 popupCloseButtonElement.addEventListener('click', togglePopupVisibility);
 
+let formElement = document.querySelector('.popup__save-btn');
+let nameInput = document.querySelector('.popup__input-name');
+let missionInput = document.querySelector('.popup__input-mission');
+let profileTitle = document.querySelector('.profile__title');
+let profileSubtitle = document.querySelector('.profile__subtitle');
+
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+    profileTitle.textContent = nameInput.value;
+    profileSubtitle.textContent = missionInput.value;
+}
+formElement.addEventListener('submit', formSubmitHandler);
+formElement.addEventListener('click', togglePopupVisibility);
