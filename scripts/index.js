92ee04f5cffe05324cf0aec5e6cf324b9fb75ee5
Romanov Eugene <effectorea@yandex.ru>
2021-11-11 19:19:14 +0300

    last of us)

--- scripts/index.js
+++ scripts/index.js
@@ -6,14 +6,16 @@ const profileEditButtonElement = document.querySelector('.profile__edit-button')
 const submitButtonElement = document.querySelector('.popup__save-btn');
 const togglePopupVisibility = function() {
     popupElement.classList.toggle('popup_opened');
+    nameInput.value = profileTitle.textContent;
+    missionInput.value = profileSubtitle.textContent;
 };
 
 profileEditButtonElement.addEventListener('click', togglePopupVisibility);
 popupCloseButtonElement.addEventListener('click', togglePopupVisibility);
 
 let formElement = document.querySelector('.popup__form');
-let nameInput = document.querySelector('.popup__input-name');
-let missionInput = document.querySelector('.popup__input-mission');
+let nameInput = document.querySelector('.popup__input_name');
+let missionInput = document.querySelector('.popup__input_mission');
 let profileTitle = document.querySelector('.profile__title');
 let profileSubtitle = document.querySelector('.profile__subtitle');
 
@@ -21,6 +23,6 @@ function formSubmitHandler (evt) {
     evt.preventDefault();
     profileTitle.textContent = nameInput.value;
     profileSubtitle.textContent = missionInput.value;
+    submitButtonElement.addEventListener('click', togglePopupVisibility);
 }
 formElement.addEventListener('submit', formSubmitHandler);
-submitButtonElement.addEventListener('click', togglePopupVisibility);
