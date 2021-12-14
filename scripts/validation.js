87f2f34ce93feedce5a99f2075a80dce3917a796
Romanov Eugene <effectorea@yandex.ru>
2021-12-14 18:11:47 +0300

    preparing for validation process

---
+++ scripts/validation.js
@@ -0,0 +1,8 @@
+enableValidation({
+    formSelector: '.popup__form',
+    inputSelector: '.popup__input',
+    submitButtonSelector: '.popup__save-btn',
+    inactiveButtonClass: 'popup__save-btn_disabled',
+    inputErrorClass: 'popup__input_type_error',
+    errorClass: 'popup__error_visible'
+  });
