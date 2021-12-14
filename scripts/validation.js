ab4fe97163a58f8f827676be45b29ca680c20666
Romanov Eugene <effectorea@yandex.ru>
2021-12-14 21:38:09 +0300

    half-way form validation process

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,3 +1,48 @@
+const showError = (form, input, errorMessageText, inputErrorClass) => {
+    const errorMessage = form.querySelector(`#${input.id}-error`);
+    errorMessage.textContent = errorMessageText;
+    input.classList.add(inputErrorClass);
+    
+}
+
+const hideError = (form, input, inputErrorClass) => {
+    const errorMessage = form.querySelector(`#${input.id}-error`);
+    errorMessage.textContent = '';
+    input.classList.remove(inputErrorClass);
+}
+
+
+const checkInputValidity = (form, input, { inputErrorClass, errorClass }) => {
+    if (!input.validity.valid) {
+        showError(form, input, input.validationMessage, inputErrorClass, errorClass);
+    } else {
+        hideError(form, input, inputErrorClass, errorClass);
+    }
+}
+
+const setInputListeners = (form, { inputSelector, ...rest }) => {
+    const inputs = form.querySelectorAll(inputSelector);
+
+    inputs.forEach((input) => {
+        input.addEventListener('input', () => {
+            checkInputValidity(form, input, rest);
+        });
+    });
+}
+
+const enableValidation = ({ formSelector, ...rest }) => {
+    const forms = document.querySelectorAll(formSelector);
+
+    forms.forEach((form) => {
+        form.addEventListener('submit', (evt) => {
+            evt.preventDefault();
+        });
+
+
+        setInputListeners(form, rest);
+    });
+}
+
 enableValidation({
     formSelector: '.popup__form',
     inputSelector: '.popup__input',
