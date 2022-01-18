2f3bd729193265c74e91adc2a57608a7ea174047
Romanov Eugene <effectorea@yandex.ru>
2022-01-18 18:13:04 +0300

    creating file structure

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
 'use strict';
 
-import Card from './Card.js';
+import Card from '../scripts/Card.js';
 import FormValidator from '../scripts/FormValidator.js';
-import Section from './Section.js';
-import PopupWithImage from './PopupWithImage.js';
-import PopupWithForm from './PopupWithForm.js';
-import UserInfo from './UserInfo.js';
+import Section from '../scripts/Section.js';
+import PopupWithImage from '../scripts/PopupWithImage.js';
+import PopupWithForm from '../scripts/PopupWithForm.js';
+import UserInfo from '../scripts/UserInfo.js';
 import {
     initialCards,
     config,
@@ -18,7 +18,7 @@ import {
     placeInput,
     imageInput,
     saveBtn
-} from './constants.js';
+} from '../scripts/constants.js';
 
 
 //получаем по экземпляру валидации форм 
